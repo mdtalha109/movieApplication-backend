382817ac47a52aecb98a419542dca2e167682317
Md Talha <mdtalha1041@gmail.com>
2022-06-01 22:44:31 +0530

    added comment

--- routes/listRoute.js
+++ routes/listRoute.js
@@ -6,13 +6,12 @@ import protect from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
-router.post('/createList',protect, createList)
-router.post('/deletelist', protect,deleteList)
-router.post('/addmovietolist', protect, AddMovieToList)
-router.post('/getmoviebylist', protect, moviesInList)
-
-router.get('/getlist/private',protect, getListPrivate)
-router.get('/getlist/public',protect, getListPublic)
+router.post('/createList',protect, createList)  // routes for creating new list
+router.post('/deletelist', protect,deleteList)   // routes for deleting new list
+router.post('/addmovietolist', protect, AddMovieToList)  // routes for adding movie to list
+router.post('/getmoviebylist', protect, moviesInList)   // routes for getting movies item in list
+router.get('/getlist/private',protect, getListPrivate)  // routes for getting list of loggedIn user
+router.get('/getlist/public',protect, getListPublic)    // routes for getting all list
 
 
 
